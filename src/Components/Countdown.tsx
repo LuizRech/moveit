c6faa95453802a2fbf663fc52195e88511f9628a
Luiz Rech <luizadre@hotmail.com>
2021-04-19 18:22:55 -0300

    creation of Context API

--- src/Components/Countdown.tsx
+++ src/Components/Countdown.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout : NodeJS.Timeout;
 
 export function Countdown(){
+  const { startNewChallenge } = useContext(ChallengeContext);
+
   const [time, setTime] = useState(0.1 * 60); //25 minutos em segundos, por isso x 60
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
@@ -32,6 +35,7 @@ export function Countdown(){
     }else if(isActive && time === 0){
       setHasFinished(true);
       setIsActive(false);
+      startNewChallenge();
     }
   }, [isActive, time]);
 
---
+++ src/contexts/ChallengeContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, ReactNode, useState } from 'react';
+
+import challenges from '../../challenges.json';
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
+
+interface ChallengeProvidersData {
+  level: number;
+  currentExperience: number; 
+  challengesCompleted: number;
+  activeChallenge: Challenge;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export const ChallengeContext = createContext({} as ChallengeProvidersData);
+
+export function ChallengesProvider({ children }: ChallengesProviderProps){
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  function levelUp(){
+    setLevel(level+1);
+  }
+
+  function startNewChallenge(){
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+
+  return (
+    <ChallengeContext.Provider value={{
+      level, 
+      currentExperience, 
+      challengesCompleted,
+      levelUp,
+      startNewChallenge,
+      activeChallenge
+    }}>
+      {children}
+    </ChallengeContext.Provider>
+  )
+}
